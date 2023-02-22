0b6dbe8bd65b96e120198392bd9eee94cbf59578
bfddeveloper <dicksonbrody@icloud.com>
2023-02-22 09:46:35 -0600

    "Finished setting up alert"

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -8,15 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var categories = [String]()
+    @State private var showingAlert = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            List(categories,id: \.self){
+                category in NavigationLink(destination: Text(category)){
+                    Text(category)
+                }
+            }
+            .navigationTitle("API Categories")
         }
-        .padding()
+        .task{
+            await getCategories()
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Loading Error"),
+            message: Text("There was a problem loading the API categories"),
+                  dismissButton: .default(Text("Ok")))
+        }
     }
+    
+    func getCategories() async {
+            let query = "https://api.publicapis.org/categories"
+            if let url = URL(string: query) {
+                if let (data, _) = try? await URLSession.shared.data(from: url) {
+                    if let decodedResponse = try? JSONDecoder().decode(Categories.self, from: data) {
+                        categories = decodedResponse.categories
+                        return
+                    }
+                }
+            }
+        showingAlert = true
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -24,3 +48,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Categories: Codable {
+    var categories: [String]
+}
+
+
